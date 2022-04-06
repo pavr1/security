d3f4b1efe31eeda69143b2265abeff14cf556b4b
Pablo Villalobos <pavr1@hotmail.com>
2022-04-06 17:04:24 -0600

    test

--- .githooks/pre-commit.go
+++ .githooks/pre-commit.go
@@ -25,6 +25,7 @@ func main() {
 	if len(os.Args) < 3 {
 		fmt.Printf("no arguments supplied in pre-commit check\n")
 		os.Exit(1)
+		return
 	}
 
 	fmt.Printf("Argument2: %s\n", os.Args[2])
@@ -34,6 +35,7 @@ func main() {
 	if fileContents, err = readFile(fileName); err != nil {
 		fmt.Printf("Error reading file for security check: %s\n", err.Error())
 		os.Exit(1)
+		return
 	}
 
 	for _, key := range keys {
--- .githooks/test.go
+++ .githooks/test.go
@@ -1,8 +1,5 @@
 package test
 
-import "fmt"
-
 func test() {
-	github_oauth_access_token := "as d"
-	fmt.Println(github_oauth_access_token)
+
 }
