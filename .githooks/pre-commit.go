b8134a6aa244e5696f74d1220568cbe325d9ae19
Pablo Villalobos <pavr1@hotmail.com>
2022-04-20 10:14:39 -0600

    test

--- .githooks/pre-commit.go
+++ .githooks/pre-commit.go
@@ -7,16 +7,17 @@ import (
 	"strings"
 )
 
-const (
-	expression = "(?i)%s\\s*(=|:=|==|===)\\s*[\"'`]{1}.*?[\"'`]"
+var (
+	expressions = []string{
+		"^[a-zA-Z0-9]+$",
+		"^[a-zA-Z0-9]:[a-zA-Z]{3}[a-zA-Z0-9]",
+	}
 )
 
 func main() {
 	var (
 		err          error
 		fileContents string
-		keys         = []string{"api_key", "shppa_", "github_token", "aws_access_key_id", "aws_secret_access_key", "aws_session_token", "github_personal_access_token", "github_oauth_access_token",
-			"github_refresh_token", "github_app_installation_access_token", "github_ssh_private_key", "shippo_live_api_token", "shopify_app_shared_secret", "shopify_access_token"}
 	)
 
 	if len(os.Args) < 3 {
@@ -31,19 +32,14 @@ func main() {
 		os.Exit(1)
 	}
 
-	for _, key := range keys {
-		fmt.Printf(" - validating '%s'...", key)
+	if isvalid, info := isValid(fileName, fileContents); !isvalid {
+		fmt.Printf("invalid! => %s\n", info)
+		os.Exit(1)
 
-		if isvalid, info := isValid(fileName, fileContents, key); !isvalid {
-			fmt.Printf("invalid! => %s\n", info)
-			os.Exit(1)
-
-			return
-		} else {
-			fmt.Println("valid!")
-		}
+		return
+	} else {
+		fmt.Println("valid!")
 	}
-
 	os.Exit(0)
 }
 
@@ -60,7 +56,7 @@ func readFile(fileName string) (string, error) {
 	return string(b), nil
 }
 
-func isValid(fileName, fileContents, key string) (bool, string) {
+func isValid(fileName, fileContents string) (bool, string) {
 	if strings.Contains(fileName, ".env") {
 		return false, "You are not allowed to commit .env files. This poses a security risk.\n"
 	}
@@ -70,15 +66,15 @@ func isValid(fileName, fileContents, key string) (bool, string) {
 		return true, ""
 	}
 
-	exp := fmt.Sprintf(expression, key)
+	for _, exp := range expressions {
+		r, _ := regexp.Compile(exp)
 
-	r, _ := regexp.Compile(exp)
+		match := r.MatchString(fileContents)
 
-	match := r.MatchString(fileContents)
-
-	if match {
-		return false, fmt.Sprintf("It looks like you are attemting to set a '%s' in %s, this is not allowed.\n", key, fileName)
-	} else {
-		return true, ""
+		if match {
+			return false, fmt.Sprintf("It looks like you are attemting to set token in file '%s'. This is not allowed\n", fileName)
+		}
 	}
+
+	return true, ""
 }
